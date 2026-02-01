126f119f0abd9d86e9cc2c8d822898eb895434c0
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-02-01 16:26:35 +0300

    Add Class clsPersonData and Method AddNewPerson

---
+++ clsPersonData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DVLD_DataAccess;
+
+namespace DVLD_DataAccess
+{
+    internal class clsPersonData
+    {
+        public static int AddNewPerson(int PersonID, ref string FirstName, ref string SecondName,
+            ref string ThirdName, ref string LastName, ref string NationalNo, ref DateTime DateOfBirth,
+            ref short Gendor, ref string Address, ref string Phone, ref string Email,
+            ref int NationalityCountryID, ref string ImagePath)
+        {
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = "INSERT INTO People (  [NationalNo], [FirstName], [SecondName], [ThirdName], " +
+                "[LastName], [DateOfBirth], [Gendor], [Address], [Phone], [Email], [NationalityCountryID]," +
+                " [ImagePath]) VALUES (@NationalNo, @FirstName, @SecondName, @ThirdName, @LastName," +
+                " @DateOfBirth, @Gendor, @Address, @Phone, @Email, @NationalityCountryID, @ImagePath);";
+
+            SqlCommand command = new SqlCommand(Query, connection);
+            command.Parameters.AddWithValue("@NationalNo", NationalNo);
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+            command.Parameters.AddWithValue("@SecondName", SecondName);
+            command.Parameters.AddWithValue("@ThirdName", ThirdName);
+            command.Parameters.AddWithValue("@LastName", LastName);
+            command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            command.Parameters.AddWithValue("@Gendor", Gendor);
+            command.Parameters.AddWithValue("@Address", Address);
+            command.Parameters.AddWithValue("@Phone", Phone);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@NationalityCountryID", NationalityCountryID);
+
+            if (string.IsNullOrEmpty(ImagePath))
+            {
+                command.Parameters.AddWithValue("@ImagePath", DBNull.Value);
+            }
+            else
+            {
+                command.Parameters.AddWithValue("@ImagePath", ImagePath);
+            }
+
+            try
+            {
+                connection.Open();
+                object newPersonID = command.ExecuteScalar();
+                if (int.TryParse(newPersonID.ToString(), out int newID))
+                {
+                    PersonID = newID;
+                }
+                else
+                {
+                    PersonID = -1;
+                }
+
+            }
+            catch { }
+            finally
+            {
+                connection.Close();
+            }
+
+            return PersonID;
+
+        }
+    }
+}
