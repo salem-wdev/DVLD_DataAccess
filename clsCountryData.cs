c8c394c7964d632b0887bb7bb4613589533267b4
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-02-04 17:55:44 +0300

    Added Class clsCountryData

---
+++ clsCountryData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsCountryData
+    {
+
+        public static DataTable GetAllCountries()
+        {
+            DataTable Table = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = "SELECT * FROM Countries";
+
+            SqlCommand Command = new SqlCommand(Query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = Command.ExecuteReader();
+
+                Table.Load(reader);
+                reader.Close();
+            }
+            catch { }
+            finally
+            {
+                connection.Close();
+            }
+
+            return Table;
+
+        }
+
+
+    }
+}
