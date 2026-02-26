8bb749a980188c0a41db91363cfd2d975651d8d4
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-02-26 15:04:02 +0300

    refactor(DataAccess): handle nullable ThirdName in GetPersonInfoByNationalNo() in clsPersonData.cs

--- clsPersonData.cs
+++ clsPersonData.cs
@@ -103,7 +103,14 @@ namespace DVLD_DataAccess
                     PersonID = Convert.ToInt32(reader["PersonID"]);
                     FirstName = reader["FirstName"].ToString();
                     SecondName = reader["SecondName"].ToString();
-                    ThirdName = reader["ThirdName"].ToString();
+                    if (reader["ThirdName"] == DBNull.Value)
+                    {
+                        ThirdName = string.Empty;
+                    }
+                    else
+                    {
+                        ThirdName = reader["ThirdName"].ToString();
+                    }
                     LastName = reader["LastName"].ToString();
                     DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]);
                     Gender = Convert.ToInt16(reader["Gendor"]);
